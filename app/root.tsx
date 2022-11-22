f2c4c88ce00a2eee72b8d396f472d56f4f19f642
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-11-22 13:10:49 -0800

    clear template index main page - prep for portfolio

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Travis Pirozzini",
   viewport: "width=device-width,initial-scale=1",
 });
 
