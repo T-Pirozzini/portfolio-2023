7fddae538d4ba94160d5e640147d9dac824c4757
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-11-24 16:10:49 -0800

    add hockey assets

--- app/routes/hockey.tsx
+++ app/routes/hockey.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import Nav from "../nav"
 
 export default function Hockey() {
   return (
-    <div>
+    <div className="bg-black font-quicksand text-white h-auto">
+      <Nav />
+      <div className="m-4">
+        <video src="../assets/hockey-dashboard-demo.mp4" width="600" autoPlay muted loop></video>
+      </div>
+      <div className="m-4">
+        <video src="../assets/nahl-original.mp4" width="600" autoPlay muted loop></video>
+      </div>      
       
     </div>
   )
