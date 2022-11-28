d3de7f8b3c82f7c8ad7365d2e36bed8936534ea9
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-11-28 11:41:00 -0800

    adjust text size across all screen sizes

--- app/routes/comb.tsx
+++ app/routes/comb.tsx
@@ -10,37 +10,37 @@ export default function Comb() {
 
       <div className="flex flex-col items-center m-4">
         <div className="text-xl font-semibold p-4">UI/UX Design</div>
-        <div className="pb-4 w-3/4">While completing the Google UI/UX Certification I was tasked with designing an application for social good. <br></br> I present to you, Comb, your shellfish harvesting companion!</div>
-        <div className="pb-4 w-3/4"><span className="font-semibold">Project Goal:</span> Food prices have gone up across the nation and time spent in nature is at record lows. We are reliant upon large food providers who also have a consequential negative impact on the environment. Comb solves both of these concerns by providing accessibility to users to secure their own food source while spending quality time in nature.</div>
+        <div className="pb-4 w-3/4 text-sm md:text-lg">While completing the Google UI/UX Certification I was tasked with designing an application for social good. <br></br> I present to you, Comb, your shellfish harvesting companion!</div>
+        <div className="pb-4 w-3/4 text-sm md:text-lg"><span className="font-semibold">Project Goal:</span> Food prices have gone up across the nation and time spent in nature is at record lows. We are reliant upon large food providers who also have a consequential negative impact on the environment. Comb solves both of these concerns by providing accessibility to users to secure their own food source while spending quality time in nature.</div>
       </div>
 
       <div className="flex flex-col items-center m-4 border-t-2">
         <div className="p-4 text-center text-xl font-semibold">User Stories</div>
-        <div className="pb-4 w-3/4 text-center">I created two users to help me visualize how my application could solve some of their barriers or pain points when it comes to shellfish harvesting.</div>
+        <div className="pb-4 w-3/4 text-center text-sm md:text-lg">I created two users to help me visualize how my application could solve some of their barriers or pain points when it comes to shellfish harvesting.</div>
         <div className="grid grid-cols-1 gap-4 m-4 md:grid-cols-2">
           <img src="../assets/userstory1-comb.png"></img>
           <img src="../assets/userstory2-comb.png"></img>
         </div>
-        <div className="pb-4 w-3/4 text-center">Users want all of their shellfish harvesting information in one location. Users are required to check weather, tide information and red tide warnings all from different websites to plan their activity.</div>
+        <div className="pb-4 w-3/4 text-center text-sm md:text-lg">Users want all of their shellfish harvesting information in one location. Users are required to check weather, tide information and red tide warnings all from different websites to plan their activity.</div>
       </div>
 
       <div className="text-xl font-semibold pt-4 text-center border-t-2">Digital Wireframe</div>
       <div className="text-md italic pb-8 text-center">Created with Figma</div>
       <div className="flex flex-col items-center m-4">
         <img src="../assets/digital-wireframe-forager.png"></img>
-        <div className="p-4 w-3/4 text-center">I did a quick ideation exercise to come up with ideas for how to address gaps identified in the competitive audit. My focus was specifically on the map feature and ensuring that additional features would not block its visibility or functionality.</div>
+        <div className="p-4 w-3/4 text-center text-sm md:text-lg">I did a quick ideation exercise to come up with ideas for how to address gaps identified in the competitive audit. My focus was specifically on the map feature and ensuring that additional features would not block its visibility or functionality.</div>
       </div>
       <div className="text-xl font-semibold pt-4 text-center border-t-2">Hi-fidelity Mockup</div>
       <div className="text-md italic pb-8 text-center">Created with Figma</div>
       <div className="flex flex-col items-center m-4 pb-4">
         <img src="../assets/mockup-comb.png"></img>
-        <div className="p-4 w-3/4 text-center m-4">Based on insights from conducting usability studies, I applied a design colour scheme that is simplistic and modern. I ensured that the home screen has white space and does not become cluttered with elements.</div>
+        <div className="p-4 w-3/4 text-center m-4 text-sm md:text-lg">Based on insights from conducting usability studies, I applied a design colour scheme that is simplistic and modern. I ensured that the home screen has white space and does not become cluttered with elements.</div>
         <img src="../assets/prototype-comb.png"></img>
       </div>
 
       <div className="flex flex-col items-center m-4 border-t-2">
         <div className="p-4 text-center text-xl font-semibold">React-Native Mobile Application</div>      
-        <div className="p-4 w-3/4 text-center">Now that the design was complete, it was time to pass it off to the web developers (also me hehe). I teamed up with Richard and we began converting my design to code! It was incredibly rewarding watching my design come to life! </div>
+        <div className="p-4 w-3/4 text-center text-sm md:text-lg">Now that the design was complete, it was time to pass it off to the web developers (also me hehe). I teamed up with Richard and we began converting my design to code! It was incredibly rewarding watching my design come to life! </div>
         <div className="grid grid-cols-1 gap-8 m-8 md:grid-cols-4">
           <img src="../assets/comb-header.png"></img>
           <img src="../assets/comb-tide.png"></img>
@@ -54,7 +54,7 @@ export default function Comb() {
         <div className="text-xl font-semibold p-4 text-center">Final Product</div>
         <img src="../assets/banner-comb.png"></img>
         <div className="m-4 text-center">
-          <div>The reason that I chose to earn a UI/UX Certificate after becoming a Full-Stack Developer was to experience the full process - from design - to implementation - and then deployment.</div>
+          <div className="text-sm md:text-lg">The reason that I chose to earn a UI/UX Certificate after becoming a Full-Stack Developer was to experience the full process - from design - to implementation - and then deployment.</div>
           <br></br>
           <div>Comb is available on the Google Play Store. Check it out - we would love to get your feedback!</div>
           <br></br>
--- app/routes/hockey.tsx
+++ app/routes/hockey.tsx
@@ -7,9 +7,9 @@ export default function Hockey() {
       <div className="font-oswald text-6xl text-center border-t-2 m-6 p-4">Nanaimo Hockey League</div>
 
       <div className="flex flex-col items-center m-4">
-        <div className="w-3/4 mb-4">I redesigned my local communities hockey league website. I kept the original design so that it would feel familiar, but improved upon the functionality and increased its web responsivity for desktop, tablet and mobile devices.</div>
+        <div className="w-3/4 mb-4 text-sm md:text-lg">I redesigned my local communities hockey league website. I kept the original design so that it would feel familiar, but improved upon the functionality and increased its web responsivity for desktop, tablet and mobile devices.</div>
         <div className="text-center w-3/4 text-xl text-semibold m-4">The Division Feature</div>
-        <div className="w-3/4 mb-4">I noticed that the Division feature could use an upgrade. I integrated same page filtering and the ability to dynamically add and remove teams if signed in as an administrator. I used Firestore for the database and Firebase for authentication and deployment of the application.</div>
+        <div className="w-3/4 mb-4 text-sm md:text-lg">I noticed that the Division feature could use an upgrade. I integrated same page filtering and the ability to dynamically add and remove teams if signed in as an administrator. I used Firestore for the database and Firebase for authentication and deployment of the application.</div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2">
         <div className="justify-self-center">
