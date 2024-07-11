fc132b642f5af5a70bf3f472766c04109152b7c4
T-Pirozzini <tpirozzini@gmail.com>
2024-07-11 16:50:20 -0700

    add new mp4 version of rts demo

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -65,8 +65,8 @@ export default function Index() {
     <div className="flex justify-center">
       <div className="pt-4">
         <video  width="300" preload="auto" autoPlay loop muted>
-        <img src="../assets/rts_demo_july2024gif.gif" width="300" alt="Demo GIF" />
-          {/* <source src="../assets/rts_demo_july2024gif.gif" alt="RTS Demo" /> */}
+        {/* <img src="../assets/rts_demo_july2024gif.gif" width="300" alt="Demo GIF" /> */}
+          <source src="../assets/rts_demo_july2024.mp4" type="video/mp4"/>
         </video>
       </div> 
     </div>        
